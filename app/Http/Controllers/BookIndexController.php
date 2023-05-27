13019a3dee4d6dd39ef7e9a121a6d5f131f39d88
Edimilson <edimilsonassis@outlook.com.br>
2023-05-27 16:50:56 -0300

    making models

---
+++ app/Http/Controllers/BookIndexController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookIndexRequest;
+use App\Http\Requests\UpdateBookIndexRequest;
+use App\Models\v1\BookIndex;
+
+class BookIndexController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookIndexRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookIndex $bookIndex)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BookIndex $bookIndex)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookIndexRequest $request, BookIndex $bookIndex)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookIndex $bookIndex)
+    {
+        //
+    }
+}
